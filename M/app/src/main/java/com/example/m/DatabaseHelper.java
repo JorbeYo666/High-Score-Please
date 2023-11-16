4e69e7aac73fcaacb98fd74c61f97268803ca56f
Maxyujy <53248752+Maxyujy@users.noreply.github.com>
2023-11-16 20:38:29 +0800

    Update DatabaseHelper.java

--- M/app/src/main/java/com/example/m/DatabaseHelper.java
+++ M/app/src/main/java/com/example/m/DatabaseHelper.java
@@ -1,53 +1,85 @@
 package com.example.m;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 import androidx.annotation.Nullable;
-import java.util.ArrayList;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "user.db";
+
+    private static final String CREATE_USER = "create table user(id integer primary key autoincrement," +
+            "idnum varchar(30)," +
+            "password varchar(30)," +
+            "tel varchar(30))";
 
 
-    public class DatabaseHelper extends SQLiteOpenHelper {
-        //创建一个数据库
-        private SQLiteDatabase db;
+    public DatabaseHelper(@Nullable Context context) {
+        super(context,DB_NAME,null,1);
+    }
 
-        public DatabaseHelper(@Nullable Context context) {
-            super(context, "db_test", null, 1);
-            db = getReadableDatabase();
-        }
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            //在第一次创建数据库的时候，创建一些字段
-            String sql = "create table user(_id integer, name varchar(50), password varchar(40))";
-            db.execSQL(sql);//sql语句的执行函数
-        }
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            //如果这个表中存在user,我们可以先把他去掉,然后重新创建
-            String sql = "DROP TABLE IF EXISTS user";
-            db.execSQL(sql);
-            onCreate(db);
-        }
-        //为使项目结构更加紧凑，我们在此类中编写增删改查的函数,因为只有登录和注册界面,因此只涉及到写入数据库insert和query的操作
-        public void insert(String name,String password ){
-            db.execSQL("insert into user(idnum,password)VALUES(?,?)",new Object[]{idnum,password});
-        }
+    //创建数据表
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_USER);
+    }
 
-        public ArrayList<User> getAllDATA(){//查询数据库
-            ArrayList<User> list = new ArrayList<User>();
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-            Cursor cursor = db.query("user",null,null,null,null,null,"name DESC");
-            while(cursor.moveToNext()){
-                int index_name = cursor.getColumnIndex("name");
-                int index_password = cursor.getColumnIndex("password");
-                String name = cursor.getString(index_name);
-                String password = cursor.getString(index_password);
-                list.add(new User(idnum,password));
+    }
+
+    //注册方法实现
+    public long RegisterActivity(User u){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("id", u.getId());
+        cv.put("username ",u.getIdnum());
+        cv.put("password",u.getPassword());
+        cv.put("phone",u.getTel());
+        long users = db.insert("user",null,cv);
+        return users;
+    }
+
+    //登录方法实现
+    public boolean LoginActivity(String username,String password){
+        SQLiteDatabase db = getWritableDatabase();
+        boolean result = false;
+        Cursor users = db.query("user", null, "username like?", new String[]{username}, null, null, null);
+        if(users!=null){
+            while (users.moveToNext()){
+                @SuppressLint("Range") String username1 = users.getString(users.getColumnIndex("username"));
+                Log.i("users", "login: "+username1);
+                String password1 = users.getString(2);
+                Log.i("users", "login: "+password1);
+                result = password1.equals(password);
+                return result;
             }
-            return list;
         }
+        return false;
+    }
+    //根据idnum和telno查找当前登录用户信息
+    public User select(String username){
+        SQLiteDatabase db = getWritableDatabase();
+        User SelectUser = new User();
+        Cursor user = db.query("user", new String[]{"username", "phone"}, "username=?", new String[]{username}, null, null, null);
+        while(user.moveToNext()){
+            @SuppressLint("Range") String uname =user.getString(user.getColumnIndex("username"));
+            @SuppressLint("Range") String phone = user.getString(user.getColumnIndex("phone"));
+            SelectUser.setIdnum(uname);
+            SelectUser.setTel(phone);
+        }
+        user.close();
+        return SelectUser;
     }
 
 
+
+}
 
