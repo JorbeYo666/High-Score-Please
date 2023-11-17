8249a18a6cbf674d6f9b39f0da2ca66d3780a138
JorbeYo666 <128609668+JorbeYo666@users.noreply.github.com>
2023-11-17 17:22:25 +0800

    nmsl

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,66 +11,54 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_my_reservation,btn_online_inquiry,btn_appoint,btn_map,btn_health_education,btn_logout;
+    private Button btn_my_reservation, btn_online_inquiry, btn_appoint, btn_map, btn_health_education, btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_my_reservation= (Button) findViewById(R.id.btn_my_reservation);
-        btn_online_inquiry= (Button) findViewById(R.id.btn_online_inquiry);
-        btn_appoint= (Button) findViewById(R.id.btn_appoint);
-        btn_map= (Button) findViewById(R.id.btn_map);
-        btn_health_education= (Button) findViewById(R.id.btn_health_education);
-        btn_logout=(Button) findViewById(R.id.btn_logout);
+        btn_my_reservation = findViewById(R.id.btn_my_reservation);
+        btn_online_inquiry = findViewById(R.id.btn_online_inquiry);
+        btn_appoint = findViewById(R.id.btn_appoint);
+        btn_map = findViewById(R.id.btn_map);
+        btn_health_education = findViewById(R.id.btn_health_education);
+        btn_logout = findViewById(R.id.btn_logout);
+        setListeners();
 
-        btn_my_reservation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MyReservationActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
 
-        btn_online_inquiry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, OnlineInquiryActivity.class);
-                startActivity(intent);
-            }
-        });
+    private void setListeners() {
+        Onclick onlick = new Onclick();
+        btn_my_reservation.setOnClickListener(onlick);
+        btn_online_inquiry.setOnClickListener(onlick);
+        btn_appoint.setOnClickListener(onlick);
+        btn_map.setOnClickListener(onlick);
+        btn_health_education.setOnClickListener(onlick);
+        btn_logout.setOnClickListener(onlick);
 
-        btn_appoint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AppointActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
 
-        btn_map.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                startActivity(intent);
-            }
-        });
+    private class Onclick implements View.OnClickListener {
 
-        btn_health_education.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HealthEducationActivity.class);
-                startActivity(intent);
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            int vId=v.getId();
+            if(vId==R.id.btn_my_reservation){
+                intent = new Intent(MainActivity.this, MyReservationActivity.class);
+            } else if (vId==R.id.btn_online_inquiry) {
+                intent = new Intent(MainActivity.this, OnlineInquiryActivity.class);
+            } else if (vId==R.id.btn_appoint) {
+                intent = new Intent(MainActivity.this, AppointmentActivity.class);
+            } else if (vId==R.id.btn_map) {
+                intent = new Intent(MainActivity.this, MapActivity.class);
+            } else if (vId==R.id.btn_health_education) {
+                intent = new Intent(MainActivity.this, HealthEducationActivity.class);
+            } else if (vId==R.id.btn_logout) {
+                intent = new Intent(MainActivity.this, LoginActivity.class);
             }
-        });
-
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+            startActivity(intent);
+        }
     }
 }
